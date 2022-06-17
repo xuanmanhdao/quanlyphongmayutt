7e4bd6a726051d833e2f64ab7dc7b3d19ba7dbce
xuanmanhdao <xuanmanhdao2001@gmail.com>
2022-06-17 19:10:15 +0700

    Fix Https

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // protected $proxies;// mặc định
+    protected $proxies = '*';// tự thêm
 
     /**
      * The headers that should be used to detect proxies.
