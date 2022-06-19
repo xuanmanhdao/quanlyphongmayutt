566088e1067633e80ac12cf323989871643677b5
xuanmanhdao <xuanmanhdao2001@gmail.com>
2022-06-19 14:09:57 +0700

    Change format array push in api login

--- public/application/Login.php
+++ public/application/Login.php
@@ -19,7 +19,7 @@ $pass = strip_tags($pass);
 $pass = addslashes($pass);
 if ($taikhoan == '' || $pass == '') {
     $trangthai = ['TaiKhoan' => $mangTaiKhoan, 'StatusCode' => '401', 'Message' => 'Dữ liệu đẩy lên rỗng'];
-    echo json_encode($trangthai);
+    echo json_encode($trangthai, JSON_UNESCAPED_UNICODE);
 } else {
     // $sql = "SELECT * FROM taikhoan, giangvien  WHERE giangvien.MaGiangVien = taikhoan.MaGiangVien and taikhoan.MaGiangVien = '". $taikhoan."' and taikhoan.MatKhau = '".$pass."'";
     // $sql = `SELECT * FROM taikhoan, giangvien  WHERE giangvien.MaGiangVien = taikhoan.MaGiangVien and taikhoan.MaGiangVien ="$taikhoan"`;
@@ -45,12 +45,13 @@ if ($taikhoan == '' || $pass == '') {
             // Print the result depending if they match
             if ($verify) {
                 // array_push($mangTaiKhoan, new TaiKhoan($row['MaGiangVien'], $row['MatKhau'], $row['Quyen'], $row['HoTen'], $row['SDT'], $row['Email']));
-                array_push($mangTaiKhoan, ['MaGV' => $row['MaGiangVien']], ['PassWord' => $row['MatKhau']], ['Quyen' => $row['Quyen']], ['TenGV' => $row['HoTen']], ['SDT' => $row['SDT']], ['Email' => $row['Email']]);
+                // array_push($mangTaiKhoan, ['MaGV' => $row['MaGiangVien']], ['PassWord' => $row['MatKhau']], ['Quyen' => $row['Quyen']], ['TenGV' => $row['HoTen']], ['SDT' => $row['SDT']], ['Email' => $row['Email']]);
+                array_push($mangTaiKhoan, array('MaGV' => $row['MaGiangVien'], 'PassWord' => $row['MatKhau'], 'Quyen' => $row['Quyen'], 'TenGV' => $row['HoTen'], 'SDT' => $row['SDT'], 'Email' => $row['Email']));
                 $trangthai = ['StatusCode' => '200', 'Message' => 'Truy cập thành công', 'Data' => $mangTaiKhoan];
-                echo json_encode($trangthai);
+                echo json_encode($trangthai, JSON_UNESCAPED_UNICODE);
             } else {
                 $trangthai = ['TaiKhoan' => $mangTaiKhoan, 'StatusCode' => '401', 'Message' => 'Thông tin tài khoản sai'];
-                echo json_encode($trangthai);
+                echo json_encode($trangthai, JSON_UNESCAPED_UNICODE);
             }
             // if (!Hash::check($pass, $row['MatKhau'])) {
             //     throw new Exception("Sai mật khẩu");
@@ -58,6 +59,6 @@ if ($taikhoan == '' || $pass == '') {
         }
     } else {
         $trangthai = ['TaiKhoan' => $mangTaiKhoan, 'StatusCode' => '401', 'Message' => 'Mã giảng viên không tồn tại'];
-        echo json_encode($trangthai);
+        echo json_encode($trangthai, JSON_UNESCAPED_UNICODE);
     }
 }
