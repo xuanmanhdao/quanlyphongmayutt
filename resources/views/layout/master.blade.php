1effe02a5a78bcc49de04d74608cf8512b2b01a4
xuanmanhdao <xuanmanhdao2001@gmail.com>
2022-09-30 18:35:08 +0700

    Push project quanlyphongmayutt on github

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <title>{{ $tieuDe }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-    <meta content="Coderthemes" name="author" />
+    <meta content="Quản lý lịch phòng máy thực hành UTT" name="description" />
+    {{-- <meta content="Coderthemes" name="author" /> --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
     {{-- <link rel="shortcut icon" href="assets/images/favicon.ico"> --}}
